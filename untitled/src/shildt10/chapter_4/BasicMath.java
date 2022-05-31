8965fbcbae00877a7186cb7ca1cab25fcb1232d2
Roman Vasilev <ninetyninebeer@gmail.com>
2022-05-31 07:40:32 +0400

    page 111

---
+++ untitled/src/shildt10/chapter_4/BasicMath.java
@@ -0,0 +1,35 @@
+package shildt10.chapter_4;
+
+public class BasicMath {
+    public static void main(String[] args) {
+        System.out.println("Simple arithmetic operations");
+        System.out.println("Operation with Integer");
+
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e= " + e);
+
+        System.out.println("Operations with type double");
+
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - da;
+        double de = -dd;
+
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de= " + de);
+
+    }
+}
---
+++ untitled/src/shildt10/chapter_4/Moduls.java
@@ -0,0 +1,16 @@
+package shildt10.chapter_4;
+
+public class Moduls{
+    public static void main(String[] args) {
+        int x = 42;
+        double d = 42.25;
+
+        System.out.println("x mod 10 = " + x % 10 );
+        System.out.println("x mod 10 = " + x % 10 );
+        System.out.println("x mod 1 = " + x % 1 );
+        System.out.println("d mod 1 = " + d % 1 );
+        System.out.println("d mod 10 = " + d % 10 );
+        System.out.println("d mod 100 = " + d % 100 );
+
+    }
+}
